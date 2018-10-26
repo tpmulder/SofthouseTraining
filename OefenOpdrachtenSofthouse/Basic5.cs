aa5078608f257ce7348bffb6cf1a4b7302f86275
ties mulder <ties-mulder@hotmail.com>
2018-10-27 01:05:41 +0200

    Progress 27-10-2018

--- OefenOpdrachtenSofthouse/Basic5.cs
+++ OefenOpdrachtenSofthouse/Basic5.cs
@@ -9,36 +9,23 @@ namespace OefenOpdrachtenSofthouse
     class Basic5
     {
         public static void Program5()
-        /* 
-        Haal het nummer achter "Main" weg en druk op start als je het programma wilt starten
-        Maar zet deze wel terug als je het programma niet meer nodig hebt
-        */
         {
-            int n, x, y;
-            List<int> list = new List<int>();
+            int n = 0;
 
-            Console.WriteLine("Basic5 - ");
+            Console.WriteLine("Basic5 - A program that solves a function");
+            Console.WriteLine("Equasion to solve: x = y² - 2y +1");
+            Console.WriteLine("The program solves the equasion for y - 2 to y + 2");
+            Console.Write("Input y: ");
 
-            for (int i = 0; i < 1; i++)
+            while (!int.TryParse(Console.ReadLine(), out n))
             {
-                Console.Write("Enter x and y: ");
-
-                while (!int.TryParse(Console.ReadLine(), out n))
-                {
-                    Console.Write("Please Enter an int: ");
-                }
-                list.Add(n);
+                Console.Write("Please Enter an int: ");
             }
-            x = list[1];
-            y = list[2];
 
-            Console.WriteLine("equasion to solve: x = y² - 2y +1");
-
-            for (y = -5; y <= 5; y++)
+            for (int y = n - 2; y <= n + 2; y++)
             {
-                x = y * y - 2 * y + 1;
-                Console.WriteLine(
-                    "y = {0} ; x=({0})² - 2*({0}) +1 = {1}",
+                int x = y * y - 2 * y + 1;
+                Console.WriteLine("y = {0} ; x=({0})² - 2*({0}) +1 = {1}",
                     y, x);
             }
         }
